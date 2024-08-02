3ea220a6e81ba9ff480d97ec589143d18c5ad655
eanton71 <eanton71@gmail.com>
2024-08-02 20:54:27 +0200

    ejercicio 4.6 blog list test mostBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,6 +8,27 @@ const favoriteBlog = (blogs) => {
     const maxLikes = Math.max(...blogs.map(blog => blog.likes))
     return blogs.find(blog => blog.likes === maxLikes)
 }
+
+function mostBlogs(blogs) {
+    const contador = blogs.reduce((acumulado, blog) => {
+        const author = blog.author;
+        if (!acumulado[author]) {
+            acumulado[author] = 1;
+        } else {
+            acumulado[author]++;
+        }
+        return acumulado;
+    }, {});
+
+    const mostBlogs = Object.keys(contador).reduce((max, author) => {
+        if (contador[author] > max.blogs) {
+            return { author, blogs: contador[author] };
+        }
+        return max;
+    }, { author: '', blogs: 0 });
+
+    return mostBlogs;
+}
 module.exports = {
-    dummy,totalLikes,favoriteBlog
+    dummy,totalLikes,favoriteBlog,mostBlogs
 }
